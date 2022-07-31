17a338cd61144631dee7edc9a2f6fa0a918fc990
GootaLK <lokosmen1235@gmail.com>
2022-07-30 19:15:15 -0500

    Feat: Add Layout to _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/layout'
+import { confirmTabType } from '@/components/nav'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithTitle = NextPage & {
+  title?: string
+}
+
+type AppPropsWithTitle = AppProps & {
+  Component: NextPageWithTitle
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithTitle) {
+  const componentName = Component.name.toLowerCase()
+
+  return (
+    <Layout
+      pageName={confirmTabType(componentName) ? componentName : 'home'}
+      title={Component.title}
+    >
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
+export { type NextPageWithTitle }
