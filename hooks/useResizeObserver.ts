c37c7b9353c177c36241c04af24aca693328b18d
GootaLK <lokosmen1235@gmail.com>
2022-07-30 19:04:04 -0500

    Feat: Create useResizeObsever hook

---
+++ hooks/useResizeObserver.ts
@@ -0,0 +1,40 @@
+import { type RefObject, useLayoutEffect, useRef } from 'react'
+
+export function useResizeObserver(
+  action: (c: any) => void,
+  ref: RefObject<HTMLElement> | RefObject<HTMLElement>[]
+) {
+  const resizer = useRef<ResizeObserver>(
+    (() => {
+      if (typeof window === 'undefined') return null
+
+      return new window.ResizeObserver((entries) => {
+        for (const entry of entries) {
+          if (entry.contentBoxSize) {
+            const contentBoxSize = Array.isArray(entry.contentBoxSize)
+              ? entry.contentBoxSize[0]
+              : entry.contentBoxSize
+            action(contentBoxSize)
+          } else {
+            action(entry.contentRect)
+          }
+        }
+      })
+    })()
+  )
+
+  useLayoutEffect(() => {
+    const resizerCopy = resizer.current
+    if (resizerCopy === null) return
+
+    if (Array.isArray(ref)) {
+      ref.forEach(({ current }) => current && resizerCopy.observe(current))
+    } else if (ref.current) {
+      resizerCopy.observe(ref.current)
+    }
+
+    return () => resizerCopy.disconnect()
+  }, [ref])
+
+  return resizer
+}
