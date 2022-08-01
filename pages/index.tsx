5de1e3caec97bc87fb4ca14c49c42f39e79ca1e6
GootaLK <lokosmen1235@gmail.com>
2022-07-31 23:54:51 -0500

    Feat: Square homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,8 +20,8 @@ const Home: NextPageWithTitle = () => {
           <h2 className={cn(utilsStyles.heading5, utilsStyles.grayText)}>
             SO, YOU WANT TO TRAVEL TO
           </h2>
-          <h1 className={utilsStyles.heading1}>SPACE</h1>
-          <p className={cn(utilsStyles.grayText, utilsStyles.bodyText)}>
+          <h1 className={cn(utilsStyles.heading1, styles.heroTitle)}>SPACE</h1>
+          <p className={utilsStyles.grayText}>
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
@@ -29,7 +29,7 @@ const Home: NextPageWithTitle = () => {
           </p>
         </header>
         <button className={styles.button}>
-          <span className={utilsStyles.heading4}>EXPLORE</span>
+          <span>EXPLORE</span>
         </button>
       </div>
     </>
