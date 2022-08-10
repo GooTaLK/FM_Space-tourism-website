021e8b5e511ec530b5d62fc5e717ba7507411cb0
GootaLK <lokosmen1235@gmail.com>
2022-08-09 21:07:36 -0500

    Fix: Use getBoundingClientRect  instead of offset

--- hooks/useActiveTabMark/index.tsx
+++ hooks/useActiveTabMark/index.tsx
@@ -16,19 +16,24 @@ function moveActiveBarToTabPosition({
   const tabDOMRef = containerDOMRef?.querySelector(`[data-tab-name='${tab}']`)
   if (!containerDOMRef || !tabDOMRef) return
 
-  const { top, left, width, height } = tabDOMRef.getBoundingClientRect()
-  const containerLeftOffset = containerDOMRef.offsetLeft
-  const containerTopOffset = containerDOMRef.offsetTop
-  const x = left - containerLeftOffset
-  const y = top - containerTopOffset
+  const tabClientRect = tabDOMRef.getBoundingClientRect()
+  const containerClientRect = containerDOMRef.getBoundingClientRect()
+  const x = tabClientRect.left - containerClientRect.left
+  const y = tabClientRect.top - containerClientRect.top
 
   if (direction === 'horizontal') {
     activeBarRef.current.style.setProperty('--x', `${x}px`)
-    activeBarRef.current.style.setProperty('--width', `${width}px`)
+    activeBarRef.current.style.setProperty(
+      '--width',
+      `${tabClientRect.width}px`
+    )
     activeBarRef.current.dataset.activeTabMark = 'horizontal'
   } else {
     activeBarRef.current.style.setProperty('--y', `${y}px`)
-    activeBarRef.current.style.setProperty('--height', `${height}px`)
+    activeBarRef.current.style.setProperty(
+      '--height',
+      `${tabClientRect.height}px`
+    )
     activeBarRef.current.dataset.activeTabMark = 'vertical'
   }
 }
